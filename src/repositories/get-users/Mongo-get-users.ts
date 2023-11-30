fec4b1b79c9d426a1fe9145114150db47bb3c6c7
Helio-junior-ADS <64769193+Helio-junior-ADS@users.noreply.github.com>
2023-11-30 15:55:38 -0300

    feat: add to id User

--- src/repositories/get-users/Mongo-get-users.ts
+++ src/repositories/get-users/Mongo-get-users.ts
@@ -1,8 +1,12 @@
 import { IGetUserRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements IGetUserRepository {
   async getUsers(): Promise<User[]> {
+    const users = await MongoClient.db.collection<User>('users').find({}).toArray();
+
+    users[0].
     return [{
       firstName: 'Hélio',
       lastName: 'Júnior',
